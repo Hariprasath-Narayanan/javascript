5866110f578583511313a45dc890a604f21aadbc
Hariprasath-Narayanan <116249569+Hariprasath-Narayanan@users.noreply.github.com>
2022-10-27 11:25:12 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,330 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('jonas');
+// console.log(23);
+
+// let firstName = "hari prasath";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let hari_prasath="HP";
+// console.log(hari_prasath);
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'hari');
+
+// javascriptIsFun='yes!';
+// console.log(typeof javascriptIsFun);
+/*
+let year;
+console.log(year);
+console.log(typeof year);
+
+year=2001;
+console.log(typeof year);
+*/
+
+// let age=30;
+// age=31;
+// math operators
+// const now = 2022;
+// const agejonas = now - 2001;
+// const agesarah = now - 2018;
+// console.log(agejonas,agesarah);
+// console.log(agejonas*2, agejonas/10,2**3);
+
+// const firstName='hari';
+// const lastName='prasath';
+// console.log(firstName+' '+lastName);
+
+// assingment operators
+// let x=10+5;
+// x +=10;
+// x*=4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+// console.log(typeof x);
+
+// comparison operators
+// console.log(agejonas>agesarah);
+// console.log(agesarah<=17);
+
+// const markWeight=78;
+// const markHeight=1.69;
+// const johnWeight=92;
+// const johnHeight=1.95;
+/*
+const markWeight=95;
+const markHeight=1.88;
+const johnWeight=85;
+const johnHeight=1.76;
+
+const BMIMark=markWeight/markHeight**2;
+const BMIjohn=johnWeight/johnHeight**2;
+const markHigherBMI=BMIMark>BMIjohn;
+console.log(markHigherBMI);
+console.log(BMIMark,BMIjohn);
+*/
+
+// const name='hari prasath';
+// const job='software engineer';
+// const birthyear=2001;
+// const year=2022;
+
+// console.log("I'm "  + name + ',a '+(year-birthyear)+'years old '+job);
+
+
+// const hariNew = `I'm ${name}, a ${year-birthyear} years old ${job}!`;
+// console.log(hariNew);
+
+// console.log(`just a regular's string...`);
+
+/*
+const age= 15;
+
+if(age >= 18) {
+    console.log('hari can start driving licence 🚙')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`hari is too young. wait another ${yearsLeft} years  :)`);
+}
+
+
+
+const birthyear = 2012;
+
+let century;
+if (birthyear <= 2000) {
+     century = 20;
+} else {
+     century = 21;
+}
+console.log(century);
+*/
+
+
+// const markWeight=78;
+// const markHeight=1.69;
+// const johnWeight=92;
+// const johnHeight=1.95;
+
+// const markWeight=95;
+// const markHeight=1.88;
+// const johnWeight=85;
+// const johnHeight=1.76;
+
+// const BMIMark=markWeight/markHeight**2;
+// const BMIjohn=johnWeight/johnHeight**2;
+// const markHigherBMI=BMIMark>BMIjohn;
+// console.log(BMIMark,BMIjohn);
+
+
+// if(BMIMark>BMIjohn){
+//     console.log(`mark's BMI (${BMIMark}) is higher than john's (${BMIjohn})`);
+// } else {
+//     console.log(`john's BMI (${BMIjohn}) is higher than mark's(${BMIMark})`);
+
+// }
+
+//type conversion
+// const inputyear = 1998;
+// console.log(Number(inputyear),inputyear);
+// console.log(Number(inputyear)+18);
+
+// console.log(Number('hari'));
+// console.log(typeof NaN);
+
+// console.log(String(23) ,25);
+
+
+// //type coercion
+// console.log('I am ' + 23 + ' year old');
+// console.log('23'+2);
+// console.log("23"-"22" +3);
+// console.log("23"*2);
+
+
+
+// let n="1" +1;
+// n=n-1;
+// console.log(n);
+
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean({}));
+// console.log(Boolean('hari'));
+// console.log(Boolean(''));
+
+
+// const money=1;
+// if(money){
+//     console.log("don't speed it all ;)");
+// }else{
+//     console.log("you should get a job!");
+
+// }
+
+
+// let height=0;
+// if (height) {
+//     console.log("YAY! height is defined");
+// }else {
+//     console.log("height is UNDIFINED");
+// }
+
+// const a=100;
+// const b=20;
+// if(a>b){
+//     console.log("a is greater than b!");
+// }else{
+//     console.log("b is greater than a!");
+// }
+
+// const age1=(Number(21));
+// const age2=(Number(22));
+// console.log(age1+age2);
+
+/*
+const age="18";
+if (age===18) console.log('you just became a adult (strict)');
+
+if (age==18) console.log('you just became a adult (loose)');
+
+
+const favourite=Number( prompt ("what is your favourite number?"));
+console.log(favourite);
+console.log(typeof(favourite));
+
+if(favourite===23){
+    console.log("cool 23 is an amazing number");
+}else if(favourite===7){
+    console.log("cool 7 is an amazing number");
+}else{
+    console.log("number is not 23 and 7")
+}
+
+if (favourite!==23) console.log("why not 23 and 7");
+*/
+
+// const hasDriverLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriverLicense && hasGoodVision);
+// console.log(hasDriverLicense || hasGoodVision);
+// console.log(!hasDriverLicense);
+
+
+// if(hasDriverLicense && hasGoodVision){
+//     console.log("hari is able to drive!");
+// }else{
+//     console.log("hari is not able to drive");
+// }
+
+// const isTired = false;
+// console.log(hasDriverLicense && hasGoodVision && isTired);
+
+
+
+// if(hasDriverLicense && hasGoodVision && !isTired){
+//     console.log("hari is able to drive!");
+// }else{
+//     console.log("hari is not able to drive");
+// }
+
+
+// const scoreDolphins = (96 + 108 + 89)/3;
+// const scoreKoalas = (88 + 91 + 110)/3;
+// console.log(scoreDolphins,scoreKoalas);
+
+// if(scoreDolphins>scoreKoalas){
+//     console.log("Dolphin win the trouphy");
+// }else if(scoreKoalas>scoreDolphins) {
+//     console.log("koalas win the trouphy");
+// }else if(scoreDolphins===scoreKoalas) {
+//     console.log("both win the trouphy");
+// }
+
+
+//bonus 1
+// const scoreDolphins = (97 + 112 + 10)/3;
+// const scoreKoalas = (109 + 95 + 10)/3;
+// console.log(scoreDolphins,scoreKoalas);
+
+
+// if(scoreDolphins>scoreKoalas&& scoreDolphins>=100){
+//     console.log("Dolphin win the trouphy");
+// }else if(scoreKoalas>scoreDolphins&& scoreKoalas>=100) {
+//     console.log("koalas win the trouphy");
+// }else if(scoreDolphins===scoreKoalas&& scoreDolphins>=100 && scoreKoalas>=100){
+//     console.log("both win the trouphy");
+// }else{
+//     console.log("no one win's the trouphy");
+// }
+
+
+// const day= "monday";
+
+// switch (day) {
+//     case 'monday':
+//         console.log("bike");
+//         console.log("bike wash");
+//         break;
+//     case 'tuesday':
+//         console.log("car");
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log("car wash");
+//         break;
+//     case 'friday':
+//         console.log("read");
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log("enjoy the weekend ");
+//         break;
+//     default:
+//         console.log("not a valid day");
+// }
+
+// const age=23;
+// age>=18 ? console.log("i like to drink wine"):
+// console.log("i like to drink water");
+
+// const drink= age >=18 ? "wine" : "water";
+// console.log(drink);
+
+// let drink2;
+// if (age>=18) {
+//     drink2="wine";
+// }else{
+//     drink2="water";
+// }
+// console.log(drink2);
+
+// console.log(`i like to drink ${age>=18 ? 'wine': "water"}`);
+
+
+
+// const bill=275;
+// const tip= bill <=300 && bill>=50 ? bill * 0.15 : bill * 0.2;
+
+// console.log(`the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
+const bill=430;
+const tip= bill<=300 && bill>=50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
